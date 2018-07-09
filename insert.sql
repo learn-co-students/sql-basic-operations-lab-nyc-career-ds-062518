23aa3dec87fb9224e50b6e435b92fd779181c2d4
Ari Horwitz <arihorwitz@Yoninas-MacBook-Air.local>
2018-07-09 11:29:25 -0400

    Done.

---
+++ insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO planets (name, color, num_of_moons,mass, rings)
+VALUES ('Mercury',	'gray',	0,	0.55,	FALSE),
+  ('Venus',	'yellow',	0,	0.82,	FALSE),
+  ('Earth',	'blue',	1,	1.00,	FALSE),
+  ('Mars',	'red',	2,	0.11,	FALSE),
+  ('Jupiter',	'orange',	53,	317.90,	FALSE),
+  ('Saturn',	'hazel',	62,	95.19,	TRUE),
+  ('Uranus',	'light blue',	27,	14.54,	TRUE),
+  ('Neptune',	'dark blue',	14,	17.15,	TRUE),
+  ('Pluto',	'brown',	5,	0.003,	FALSE);
